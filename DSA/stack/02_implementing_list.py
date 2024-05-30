03bb4a8546152a28470f0ae1a09b8f0820bf36c2
Arrjoon <nepaliarjun049@gmail.com>
2024-05-31 01:34:14 +0545

    stack program added

---
+++ DSA/stack/02_implementing_list.py
@@ -0,0 +1,15 @@
+#python program to 
+#demonstate stack implemenation
+
+stack = []
+stack.append('a')
+stack.append('b')
+stack.append('c')
+stack.append('d')
+
+print(stack)
+
+print("Elements pop from the stack:")
+
+print(stack.pop())
+print(stack)
